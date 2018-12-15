f79360a190d0125089299e17daa5cae5ecfedb53
Benjamin Young <36654544+Nebyoung@users.noreply.github.com>
2018-12-14 19:27:05 -0600

    Add files via upload

---
+++ nameScript.py
@@ -0,0 +1,12 @@
+import discord
+TOKEN = 'NTIzMzAwNDkxNzcwMjAwMDY2.DvXhag.UhsHyNYExtCS-jt2m7gCJiXgO7U'
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+        if message.author == client.user:
+                return
+        if message.content.contains("sarah" or "SARAH" or "Sarah"):
+                message = 'I can\'t see your comment {0.author.mention}? It\'s just blank?'.format(message)
+                await.client.send_message(message.channel, msg)
+
